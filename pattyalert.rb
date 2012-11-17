f7045098414b93b5be19f83b649fd635688bb06f
Colby Rabideau <crabideau5691@gmail.com>
2012-11-17 18:55:12 -0500

    added views for statuses
    try adding ?levels=low|medium|high

--- pattyalert.rb
+++ pattyalert.rb
@@ -3,6 +3,13 @@ require 'mongo'
 
 db = Mongo::Connection.new.db("patty")
 
+levels = ['low', 'medium', 'high']
+messages = {
+  'low' => 'There probably won\'t be any patties today...',
+  'medium' => 'There might be Patties later... We\'ll keep you posted!',
+  'high' => 'There\'s Chicken Patties in the cafeteria!!!'
+}
+
 statuses = {
   'low' => 'No Chicken Patties today :(',
   'medium' => 'Today could be the day...',
@@ -10,7 +17,8 @@ statuses = {
 }
 
 get '/' do
-  @level = params[:level] ? params[:level] : 'low'
+  @level = (levels.include? params[:level]) ? params[:level] : 'low'
   @status = statuses[@level]
+  @message = messages[@level]
   erb :index
 end
