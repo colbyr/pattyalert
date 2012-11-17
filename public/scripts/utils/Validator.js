53e0b11322777a190af2884a58b22e726bd5a0d2
Colby Rabideau <crabideau5691@gmail.com>
2012-11-16 23:41:42 -0500

    validator function

--- public/scripts/utils/Validator.js
+++ public/scripts/utils/Validator.js
@@ -4,12 +4,22 @@ define([
 
   return {
 
-    validate: function () {
+    check: function (rule, value) {
+      return this.rules[rule].test(value);
+    },
+
+    validate: function (data) {
       if (!this.rules) {
         throw new Error('Validator.validate: rules object not found');
       }
 
-      return false;
+      var valid = true;
+      for (var k in data) {
+        if (this.rules.hasOwnProperty(k) && !this.check(k, data[k])) {
+          valid = false;
+        }
+      }
+      return valid;
     }
 
   };
