82e2527f0102f319faab206eca398bf169dcc82f
Colby Rabideau <crabideau5691@gmail.com>
2012-11-17 18:38:42 -0500

    set the status

--- pattyalert.rb
+++ pattyalert.rb
@@ -3,8 +3,14 @@ require 'mongo'
 
 db = Mongo::Connection.new.db("patty")
 
+statuses = {
+  'low' => 'No Chicken Patties today :(',
+  'medium' => 'Today could be the day...',
+  'high' => 'EAT ALL THE CHICKEN PATTIES'
+}
+
 get '/' do
-  @level = params[:level]
-  puts @level
-  erb(:index)
+  @level = params[:level] ? params[:level] : 'low'
+  @status = statuses[@level]
+  erb :index
 end
