2128a35e02112a1d44e649fbde7b0be4b14c8075
Justin Svegliato <justinsvegliato@Justin-Svegliatos-MacBook-Pro.local>
2012-11-17 14:17:16 -0500

    Offerings now contained in a dictionary

--- menuscraper.py
+++ menuscraper.py
@@ -17,13 +17,13 @@ def expand_meal_abbreviation(meal_abbreviation):
 def search(food):
   url = 'http://maristdining.com/dining/WeeklyMenu.htm';
   soup = BeautifulSoup(urlopen(url))
-  offerings = []
+  offerings = [] 
   for element in soup.find_all('span', text=food):
     parent = element.parent
     while not re.search(r'monday|tuesday|wednesday|thursday|friday|saturday|sunday', str(parent)):
       parent = parent.parent
     meal = expand_meal_abbreviation(str(element.parent.parent.parent['class'][0]))
-    offerings.append([str(parent['id']), meal])     
+    offerings.append({'day': str(parent['id']), 'meal': meal})     
   return offerings
   
 def main():
