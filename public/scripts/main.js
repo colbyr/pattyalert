d415b3e422c5267b34b1d5207dd5125ceb4860a2
Colby Rabideau <crabideau5691@gmail.com>
2012-11-17 12:24:54 -0500

    some styles

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,15 @@
 define([
   'Signup',
   'vendor/ready',
+  'vendor/skrollr',
   'vendor/underscore'
-], function (Signup, ready) {
+], function (Signup, ready, skrollr) {
 
   window.PattyAlert = {};
 
   ready(function () {
     PattyAlert.signup = new Signup('signup', 'signup');
+    PattyAlert.skrollr = skrollr.init();
   });
 
 });
