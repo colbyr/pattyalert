f1aec1c52e504a6d91f27c3e73b6da99d7894add
Dan Miller <dan@danielsmiller.com>
2012-11-19 19:04:44 -0500

    Broke out MongoDB connection into its own static class.

---
+++ models/mongo_connection.rb
@@ -0,0 +1,15 @@
+require 'mongo'
+require 'uri'
+
+class MongoConnection
+
+  def self.get_connection
+    return @db_connection if @db_connection
+    db = URI.parse(ENV['MONGOHQ_URL'])
+    db_name = db.path.gsub(/^\//, '')
+    @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+    @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+    @db_connection
+  end
+
+end
